cb546e16909e8b1bdedd1d713e0750133a477181
Mitul Sharma <misharma@redhat.com>
2024-05-14 11:30:32 +0530

    create webhook script

---
+++ main.py
@@ -0,0 +1,24 @@
+from json import dumps
+
+from httplib2 import Http
+
+
+def main():
+    """Hangouts Chat incoming webhook quickstart."""
+    url = 'put your google chat space url here'
+    bot_message = {
+        'text': 'Hello from a Python script!'}
+    message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
+    http_obj = Http()
+    response = http_obj.request(
+        uri=url,
+        method='POST',
+        headers=message_headers,
+        body=dumps(bot_message),
+    )
+    print(response)
+
+
+if __name__ == '__main__':
+    main()
+
