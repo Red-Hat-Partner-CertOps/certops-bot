b0e6fe77fdf0fa368289ea14e6a850a93b4f7526
Mitul Sharma <misharma@redhat.com>
2024-05-15 16:39:55 +0530

    add card content

--- main.py
+++ main.py
@@ -8,8 +8,48 @@ from httplib2 import Http
 def main():
     """Hangouts Chat incoming webhook quickstart."""
     url = 'put your google chat space url here'
-    bot_message = {
-        'text': 'Hello from a Python script!'}
+    # Constructing the card content
+    card_content = {
+        "cards": [
+            {
+                "sections": [
+                    {
+                        "widgets": [
+                            {
+                                'textParagraph': {
+                                    'text': 'Hello team, Please fill your Daily Standup Updates'
+                                }
+                            },
+                            {
+                                "buttons": [
+                                    {
+                                        "textButton": {
+                                            "text": "Open Document",
+                                            "onClick": {
+                                                "openLink": {
+                                                    "url": "https://docs.google.com/document/d/1sjHfSmm0_0DF0Ax7An4DDsqu0B0yjxDksPwuzMaVtMA/edit#heading=h.ny8eurgr6hhm"
+                                                }
+                                            }
+                                        }
+                                    },
+                                    {
+                                        "textButton": {
+                                            "text": "Join Google Meet",
+                                            "onClick": {
+                                                "openLink": {
+                                                    "url": "https://meet.google.com/eqt-hzcy-hrm?authuser=0"
+                                                }
+                                            }
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
     message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
     http_obj = Http()
     response = http_obj.request(
